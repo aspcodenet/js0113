cb0e8bf5fcca7b9b5440a4c0c96f76ddf51af14b
Stefan Holmberg <stefan.holmberg@systementor.se>
2025-01-15 10:58:09 +0100

    eslint

--- src/data/post.js
+++ src/data/post.js
@@ -1,42 +1,36 @@
-import axios from "axios";
-
+import axios from 'axios';
 
 const url = 'http://localhost:3000/posts';
 
+export async function loadPosts() {
+  const posts = await axios.get(url);
+  return posts.data;
+  // const response = await fetch(url);
+  // const posts = await response.json();
 
-export async function loadPosts(){
-    const posts = await axios.get(url)
-    return posts.data
-    // const response = await fetch(url);
-    // const posts = await response.json();
-  
-    // return posts
+  // return posts
 }
 
-
 export async function addPost(blogpost) {
-    const response = await axios.post(url,blogpost, {
-        headers: {
-            'Accept': 'application/json, text/plain',
-            'Content-Type': 'application/json;charset=UTF-8'
-        }
-    });
-  }  
-
-
-  export async function loadPost(id) {
-    const response = await axios.get(url + '/' + id);
-    const players = response.data;
-    return players
-  }
-
-
-
-  export async function updatePost(blogpost) {
-    const response = await axios.put(url + '/' + blogpost.id,blogpost, {
-      headers: {
-          'Accept': 'application/json, text/plain',
-          'Content-Type': 'application/json;charset=UTF-8'
-      }
+  await axios.post(url, blogpost, {
+    headers: {
+      Accept: 'application/json, text/plain',
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
   });
-}    
+}
+
+export async function loadPost(id) {
+  const response = await axios.get(`${url}/${id}`);
+  const players = response.data;
+  return players;
+}
+
+export async function updatePost(blogpost) {
+  await axios.put(`${url}/${blogpost.id}`, blogpost, {
+    headers: {
+      Accept: 'application/json, text/plain',
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+  });
+}
